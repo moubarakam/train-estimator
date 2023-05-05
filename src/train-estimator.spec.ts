78a43f81b639d6735d21c882758bdc013c313979
moubarakam <abdoulmoubarak.yameogo@ynov.com>
2023-05-05 19:00:46 +0200

    Unit tests on Exceptions throws (Rules of the trade)

--- src/train-estimator.spec.ts
+++ src/train-estimator.spec.ts
@@ -1,6 +1,49 @@
+import { TrainTicketEstimator } from './train-estimator';
+describe('TrainTicketEstimator ==> Rules of the trade', () => {
+  let estimator: TrainTicketEstimator;
 
-describe("train estimator", function () {
-    it("should work", () => {
-        expect(1 + 2).toBe(3);
-    });
+  beforeEach(() => {
+    estimator = new TrainTicketEstimator();
+  });
+  it('should return 0 when no passengers', async () => {
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: new Date() },
+      passengers: [],
+    };
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toBe(0);
+  });
+
+  it('should throw InvalidTripInputException when start city is invalid', async () => {
+    const tripRequest = {
+      details: { from: '', to: 'Lyon', when: new Date() },
+      passengers: [{ age: 30, discounts: [] }],
+    };
+    await expect(estimator.estimate(tripRequest)).rejects.toThrowError(
+      'Start city is invalid'
+    );
+  });
+
+  it('should throw InvalidTripInputException when destination city is invalid', async () => {
+    const tripRequest = {
+      details: { from: 'Paris', to: '', when: new Date() },
+      passengers: [{ age: 30, discounts: [] }],
+    };
+    await expect(estimator.estimate(tripRequest)).rejects.toThrowError(
+      'Destination city is invalid'
+    );
+  });
+
+  it('should throw InvalidTripInputException when date is invalid', async () => {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 30, discounts: [] }],
+    };
+    await expect(estimator.estimate(tripRequest)).rejects.toThrowError(
+      'Date is invalid'
+    );
+  });
 });
