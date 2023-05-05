a4f7e98115ae99c5fa9b61fbab5c5284cdcb9794
moubarakam <abdoulmoubarak.yameogo@ynov.com>
2023-05-05 19:26:48 +0200

    Unit tests on Date of trip (+ API Mock)

--- src/train-estimator.spec.ts
+++ src/train-estimator.spec.ts
@@ -47,3 +47,85 @@ describe('TrainTicketEstimator ==> Rules of the trade', () => {
     );
   });
 });
+
+describe('TrainTicketEstimator ===> Date of trip', () => {
+  let estimator: TrainTicketEstimator;
+
+  beforeEach(() => {
+    estimator = new TrainTicketEstimator();
+    jest
+      .spyOn(estimator, 'fetchApiPrice')
+      .mockReturnValue(Promise.resolve(100));
+  });
+
+  it('30 days before the trip, we apply -20% discount (+20% for adult passenger)', async () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 30);
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 30, discounts: [] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toEqual(100);
+  });
+
+  it('We apply 2% increase per day for 25 days (so -18% at 29 days from the departure date) (+20% for adult passenger)', async () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 29);
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 30, discounts: [] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toEqual(102);
+  });
+
+  it('We apply 2% increase per day for 25 days (so 0% at 20 days from the departure date) (+20% for adult passenger)', async () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 20);
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 30, discounts: [] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toEqual(120);
+  });
+
+  it('Within 5 days of travel, the fare doubles. These rules only apply to adult passengers (+20% for adult passenger)', async () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 5);
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 30, discounts: [] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toEqual(220);
+  });
+
+  it('Within 5 days of travel, the fare doubles. These rules do not apply to fixed price tickets', async () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 5);
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 2, discounts: [] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toEqual(9);
+  });
+  it('Within 5 days of travel, the fare doubles. These rules do not apply to fixed price tickets', async () => {
+    const date = new Date();
+    date.setDate(date.getDate() + 5);
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 2, discounts: [] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toEqual(9);
+  });
+});
