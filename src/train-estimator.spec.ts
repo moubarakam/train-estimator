8b7afb02880ed0f37c91dc0bf52fb495ced4306d
moubarakam <abdoulmoubarak.yameogo@ynov.com>
2023-05-05 19:46:42 +0200

    Unit test on Discount cards

--- src/train-estimator.spec.ts
+++ src/train-estimator.spec.ts
@@ -1,4 +1,6 @@
+import { DiscountCard } from './model/trip.request';
 import { TrainTicketEstimator } from './train-estimator';
+
 describe('TrainTicketEstimator ==> Rules of the trade', () => {
   let estimator: TrainTicketEstimator;
 
@@ -205,5 +207,83 @@ describe('TrainTicketEstimator ==> Passengers type', () => {
     const price = await estimator.estimate(tripRequest);
 
     expect(price).toEqual(60);
+  });
+});
+
+describe('TrainTicketEstimator ==> Discount cards', () => {
+  let estimator: TrainTicketEstimator;
+  let date: Date;
+
+  beforeEach(() => {
+    estimator = new TrainTicketEstimator();
+    date = new Date();
+    date.setDate(date.getDate() + 20);
+    jest
+      .spyOn(estimator, 'fetchApiPrice')
+      .mockReturnValue(Promise.resolve(100));
+  });
+  it('TrainStroke staff card: all tickets are 1 euro', async () => {
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 30, discounts: [DiscountCard.TrainStroke] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toBe(1);
+  });
+  it('Senior Card: valid only if the user is over 70 years old. 20% additional discount (-20% for senior passenger)', async () => {
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 72, discounts: [DiscountCard.Senior] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toBe(60);
+  });
+
+  it('Couple Card: valid only if the trip involves 2 adult passengers. 20% discount on the ticket of each of these passengers. Valid only once!', async () => {
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [
+        { age: 30, discounts: [DiscountCard.Couple] },
+        { age: 30, discounts: [] },
+      ],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toBe(200);
+  });
+
+  it('should not apply the Couple discount for two child passengers (-40% for each minor (Up to 18 years old))', async () => {
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [
+        { age: 16, discounts: [] },
+        { age: 10, discounts: [DiscountCard.Couple] },
+      ],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toBe(120);
+  });
+
+  it('Mid-couple card: valid only if the trip involves 1 adult passenger. 10% discount on the trip. (+20% for adult passenger)', async () => {
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 30, discounts: [DiscountCard.HalfCouple] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toBe(110);
+  });
+
+  it('should not apply the HalfCouple discount for one child passenger. (-40% for each minor (Up to 18 years old))', async () => {
+    const tripRequest = {
+      details: { from: 'Paris', to: 'Lyon', when: date },
+      passengers: [{ age: 16, discounts: [DiscountCard.HalfCouple] }],
+    };
+
+    const price = await estimator.estimate(tripRequest);
+    expect(price).toBe(60);
   });
 });
